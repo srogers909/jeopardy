c4dfa399276e8bd33df44b94ef620bbd0e8fce45
srogers909 <steven.rogers909@gmail.com>
2018-06-24 14:28:01 -0500

    Rearrangements

--- src/components/gameBoard.component.ts
+++ src/components/gameBoard.component.ts
@@ -1,33 +1,29 @@
 import { IComponentController, ILogService, IQService, ITimeoutService, copy } from "angular";
-import { ICategory, IGameBoard, ITile, IGameEngine } from "../app.interfaces";
+import { ICategory, IGameBoard, ITile, IGameEngine, IClueOptions, IClue } from "../app.interfaces";
 
 let template: string = `
-    <div class="game-board row">
-        <div class="col"></div>
-        <div class="col-10">
-            <table class="table">
-                <thead>
-                    <tr scope="col" class="row">
-                        <th 
-                            class="col tile-header align-middle align-content-center"
-                            ng-repeat="category in $ctrl.categories track by category.id">
-                            <div class="header-title text-center">{{ ::category.title }}</div></th>                
-                    </tr>
-                    <tr><td colspan="6" class="spacer">&nbsp;</td></tr>
-                </thead>                
-                <tbody>
-                    <tr ng-repeat="tiles in $ctrl.gameBoard track by $index" class="row">
-                        <td 
-                            ng-repeat="tile in tiles track by $index"
-                            class="col tile align-middle align-content-center"
-                            ng-click="$ctrl.gameEngine.getClues({ value: tile.value, category: tile.category.id })">
-                            <div class="tile-title text-center">{{ ::tile.title }}</div>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-        <div class="col"></div>
+    <div class="game-board">
+        <table class="table">
+            <thead>
+                <tr scope="col" class="row">
+                    <th 
+                        class="col tile-header align-middle align-content-center"
+                        ng-repeat="category in $ctrl.categories track by category.id">
+                        <div class="header-title text-center">{{ ::category.title.trim() }}</div></th>                
+                </tr>
+                <tr><td colspan="6" class="spacer">&nbsp;</td></tr>
+            </thead>                
+            <tbody>
+                <tr ng-repeat="tiles in $ctrl.gameBoard track by $index" class="row">
+                    <td 
+                        ng-repeat="tile in tiles track by $index"
+                        class="col tile align-middle align-content-center"
+                        ng-click="$ctrl.getClue({ value: tile.value, category: tile.category.id })">
+                        <div class="tile-title text-center">{{ ::tile.title.trim() }}</div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 `;
 
@@ -42,6 +38,7 @@ class GameBoard implements IComponentController, IGameBoard {
     categories: Array<ICategory> = [];
     gameBoard: Array<ITile> = [];
     clueValues: Array<number> = [200, 400, 600, 800, 1000];
+    currentClue: IClue = null;
 
     constructor($q: IQService, $log: ILogService, $timeout: ITimeoutService, gameEngine: IGameEngine) {
         this.gameEngine = gameEngine;
@@ -72,10 +69,8 @@ class GameBoard implements IComponentController, IGameBoard {
 
                     // each categories we construct an ITile object and add to the row.
                     for (let j = 0, len = this.categories.length; j < len; j++) {
-                        this.categories[j].title = this.categories[j].title.trim();
-
                         row.push({
-                            title: `\$${this.clueValues[i]}`.trim(),
+                            title: `\$${this.clueValues[i]}`,
                             category: this.categories[j],
                             value: this.clueValues[i]
                         });
@@ -87,6 +82,13 @@ class GameBoard implements IComponentController, IGameBoard {
 
                 this.gameBoard = copy(board);
                 this.$log.info('this.gameBoard: ', this.gameBoard);
+            });
+    }
+
+    getClue(clue: IClueOptions): void {
+        this.gameEngine.getClues(clue)
+            .then((response: any) => {
+                this.currentClue = response.data as IClue;
             });
     }
 }
--- src/components/home.component.ts
+++ src/components/home.component.ts
@@ -2,44 +2,38 @@ import {IComponentController, ILogService, ITimeoutService} from "angular";
 import {IGameEngine} from "../app.interfaces";
 
 let componentTemplate: string = `
-<div class="home">
-    <div class="row" >
-        <div class="col-2">&nbsp;</div>
-        <div class="col-8">
-            <div class="title-screen" >
-                <h1>Jeopardy!</h1>
-                <div ng-if="!$ctrl.GameEngine.isGameRunning">                    
-                    <p>A game made with love using AngularJS, Webpack & Typescript.</p>
-                    <button
-                        class="btn btn-primary"
-                        ng-click="$ctrl.GameEngine.startGame()">Start Game</button>
-                </div>
-                <div ng-if="$ctrl.GameEngine.isGameRunning">
-                    <ng-form
-                        name="nameForm"
-                        id="nameForm"
-                        novalidate>
-                        
-                        <div class="input-group">
-                            <input
-                                type="text"
-                                class="form-control"
-                                ng-model="$ctrl.GameEngine.Name"
-                                placeholder="Enter your name"
-                                required />
-                            <span class="input-group-btn">
-                                <button
-                                    class="btn btn-primary"
-                                    ng-click="$ctrl.go()">Go!</button>
-                            </span>
-                        </div>
-                    </ng-form>
-                </div>
+    <div class="home">    
+        <div class="title-screen" >
+            <h1>Jeopardy!</h1>
+            <div ng-if="!$ctrl.GameEngine.isGameRunning">                    
+                <p>A game made with love using AngularJS, Webpack & Typescript.</p>
+                <button
+                    class="btn btn-primary"
+                    ng-click="$ctrl.GameEngine.startGame()">Start Game</button>
             </div>
-        </div>
-        <div class="col-2">&nbsp;</div>        
-    </div>       
-</div>
+            <div ng-if="$ctrl.GameEngine.isGameRunning">
+                <ng-form
+                    name="nameForm"
+                    id="nameForm"
+                    novalidate>
+                    
+                    <div class="input-group">
+                        <input
+                            type="text"
+                            class="form-control"
+                            ng-model="$ctrl.GameEngine.Name"
+                            placeholder="Enter your name"
+                            required />
+                        <span class="input-group-btn">
+                            <button
+                                class="btn btn-primary"
+                                ng-click="$ctrl.go()">Go!</button>
+                        </span>
+                    </div>
+                </ng-form>
+            </div>
+        </div>     
+    </div>
 `;
 
 export interface iHome {
